03ff7eea85e8b56d2f1d256cd5dfb5adc938ee7d
Sczeam <zawhtikeaung19@gmail.com>
2025-07-10 03:08:33 +0100

    Remove Orders tab and related functionality from Settings component

--- src/lib/components/admin/Settings.svelte
+++ src/lib/components/admin/Settings.svelte
@@ -21,7 +21,6 @@
 	import Evaluations from './Settings/Evaluations.svelte';
 	import CodeExecution from './Settings/CodeExecution.svelte';
 	import Tools from './Settings/Tools.svelte';
-	import Orders from './Settings/Orders.svelte';
 
 	const i18n = getContext('i18n');
 
@@ -120,36 +119,6 @@
 				</svg>
 			</div>
 			<div class=" self-center">{$i18n.t('Models')}</div>
-		</button>
-
-		<button
-			class="px-0.5 py-1 min-w-fit rounded-lg flex-1 md:flex-none flex text-left transition {selectedTab ===
-			'orders'
-				? ''
-				: ' text-gray-300 dark:text-gray-600 hover:text-gray-700 dark:hover:text-white'}"
-			on:click={() => {
-				selectedTab = 'orders';
-			}}
-		>
-			<div class=" self-center mr-2">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="currentColor"
-					class="w-4 h-4"
-				>
-					<path d="M12 7.5a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
-					<path
-						fill-rule="evenodd"
-						d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 14.625v-9.75ZM8.25 9.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM18.75 9a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.008a.75.75 0 0 1-.75-.75V9.75a.75.75 0 0 1 .75-.75h.008ZM4.5 9.75A.75.75 0 0 1 5.25 9h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H5.25a.75.75 0 0 1-.75-.75V9.75Z"
-						clip-rule="evenodd"
-					/>
-					<path
-						d="M2.25 18a.75.75 0 0 0 0 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 0 0-.75-.75H2.25Z"
-					/>
-				</svg>
-			</div>
-			<div class=" self-center">{$i18n.t('Orders')}</div>
 		</button>
 
 		<button
@@ -429,12 +398,14 @@
 			/>
 		{:else if selectedTab === 'models'}
 			<Models />
-		{:else if selectedTab === 'orders'}
-			<Orders />
 		{:else if selectedTab === 'evaluations'}
 			<Evaluations />
 		{:else if selectedTab === 'tools'}
-			<Tools />
+			<Tools
+				saveSettings={async () => {
+					toast.success($i18n.t('Settings saved successfully!'));
+				}}
+			/>
 		{:else if selectedTab === 'documents'}
 			<Documents
 				on:save={async () => {
